a7a1daa72491a19c1d75668b3dd2d2348feda278
EugeneDokyeAnokye <yawanokye99@gmail.com>
2025-12-12 10:42:51 +0000

    fix: added api for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,22 +89,22 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    #     'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('DATABASE_NAME'),
-    #     'USER': os.environ.get('DATABASE_USER'),
-    #     'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-    #     'HOST': os.environ.get('DATABASE_HOST'),
-    #     'PORT': os.environ.get('DATABASE_PORT'),
-    # }
         'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urbanvital',
-        'USER': 'postgres',
-        'PASSWORD': 'Final@2025',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'urbanvital',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Final@2025',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
