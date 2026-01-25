91ce378b2a3012ed488ed889ed0a3d6ddbd8d0d1
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-01-25 12:39:41 +0000

    feat: update database settings to use PostgreSQL and remove superuser creation script

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,8 +120,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3', 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
