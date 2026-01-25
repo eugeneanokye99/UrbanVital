a88ca5acd4d33356f7ef0e51d5c4ac4aac659fe6
EugeneDokyeAnokye <yawanokye99@gmail.com>
2026-01-25 12:11:55 +0000

    fix: update superuser creation logic to check for 'urbanvital' username

--- backend/superuser.py
+++ backend/superuser.py
@@ -1,7 +1,13 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-if not User.objects.filter(username="admin").exists():
+if not User.objects.filter(username="urbanvital").exists():
     User.objects.create_superuser(
         username="urbanvital",
         email="felanotechnologies@gmail.com",
